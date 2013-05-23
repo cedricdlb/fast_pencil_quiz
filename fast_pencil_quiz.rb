e10a118d252f20353fdf4bb482e428a062c7bd89
Cedric de La Beaujardiere <cdlb.git@gmail.com>
2013-05-23 12:23:18 -0700

    Completed implementation and fixed up tests

--- fast_pencil_quiz.rb
+++ fast_pencil_quiz.rb
@@ -1,24 +1,84 @@
 #!/usr/bin/ruby
 
 class FastPencilQuiz
-	attr_reader :q_and_a_candidates
+	attr_accessor :q_and_a_candidates
+	attr_accessor :dictionary_file_name, :questions_file_name, :answers_file_name
+	attr_accessor :dictionary_file,      :questions_file,      :answers_file
 
-	def initialize()
-		@q_and_a_candidates = {}
+	def initialize
+		@q_and_a_candidates = Hash.new {|hash, key| hash[key] = []}
 	end
 
-	def run()
+	def run
+		if process_command_line
+			open_files
+			determine_unique_sequences if find_sequences > 0
+			close_files
+		end
 	end
 
-	def find_sequences(dictionary_file)
+	def process_command_line
+		were_inputs_ok = true
+		unless File.exists?(dictionary_file_name) && File.file?(dictionary_file_name) && File.readable?(dictionary_file_name)
+			puts "FAILURE: dictionary_file_name #{dictionary_file_name} does not exist, is not a proper file, or is not readable..."
+			were_inputs_ok = false
+		end
+		
+		were_inputs_ok
 	end
 
-	def questions_file_name()
+	def find_sequences
+		@dictionary_file.each do |word|
+			scan_limit = word.length - 4
+			if scan_limit >= 0
+				(0..scan_limit).each do |i|
+					sequence = word[i,4]
+					q_and_a_candidates[sequence] << word if sequence =~ /[a-zA-Z]{4}/
+				end
+			end
+		end
+		q_and_a_candidates.size
 	end
 
-	def answers_file_name()
+	def determine_unique_sequences
+		q_and_a_candidates.each do |sequence, word_list|
+			if 1 == word_list.length
+				questions_file.puts(sequence)
+				answers_file.puts(word_list[0])
+			end
+		end
 	end
 
-	def determine_unique_sequences()
+	def open_files
+		@dictionary_file = File.open(dictionary_file_name, "r")
+		@questions_file = File.open(questions_file_name, "w")
+		@answers_file = File.open(answers_file_name, "w")
+	end
+
+	def close_files
+		@dictionary_file.close
+		@questions_file.close
+		@answers_file.close
+	end
+
+	def dictionary_file_name
+		@dictionary_file_name ||= ARGV.shift
+		@dictionary_file_name
+	end
+
+	def questions_file_name
+		unless @questions_file_name
+			@base_name ||= File.basename(dictionary_file_name, ".*")
+			@questions_file_name = File.join(File.dirname(dictionary_file_name), "#{@base_name}.questions.txt")
+		end
+		@questions_file_name
+	end
+
+	def answers_file_name
+		unless @answers_file_name
+			@base_name ||= File.basename(dictionary_file_name, ".*")
+			@answers_file_name = File.join(File.dirname(dictionary_file_name), "#{@base_name}.answers.txt")
+		end
+		@answers_file_name
 	end
 end
--- spec/fast_pencil_quiz_spec.rb
+++ spec/fast_pencil_quiz_spec.rb
@@ -2,11 +2,10 @@
 require_relative '../fast_pencil_quiz.rb'
 
 describe FastPencilQuiz do
-	before do
+	before(:each) do
 		@fast_pencil_quiz = FastPencilQuiz.new
+		subject { @fast_pencil_quiz }
 	end
-
-	subject { @fast_pencil_quiz }
 
 	context "when #find_sequences has not yet been run" do
 		subject { @fast_pencil_quiz.q_and_a_candidates }
@@ -14,103 +13,94 @@ describe FastPencilQuiz do
 		it { should be_empty }
 	end
 
-	context "when #run is run" do
-		before do
-			@dictionary_file_name = "path to dictionary_file"
-			@mock_file_dictionary = mock(File)
-			@fast_pencil_quiz.should_receive(:dictionary_file_name).and_return(@dictionary_file_name)
-			File.should_receive(:open).with(@dictionary_file_name, "r").and_yield(@mock_file_dictionary)
-			@fast_pencil_quiz.run()
+	context "when #run" do
+		before(:each) do
+			@dictionary_file_name = "dictionary_file.txt"
+			@questions_file_name = "./dictionary_file.questions.txt"
+			@answers_file_name   = "./dictionary_file.answers.txt"
+			ARGV << @dictionary_file_name
+			@dictionary_mock_file = mock(File)
+			@questions_mock_file = mock(File)
+			@answers_mock_file   = mock(File)
+			File.should_receive(:exists?).with(@dictionary_file_name).and_return(true)
+			File.should_receive(:file?).with(@dictionary_file_name).and_return(true)
+			File.should_receive(:readable?).with(@dictionary_file_name).and_return(true)
+			@fast_pencil_quiz.should_receive(:dictionary_file_name).any_number_of_times.and_return(@dictionary_file_name)
+			@fast_pencil_quiz.should_receive(:questions_file_name).and_return(@questions_file_name)
+			@fast_pencil_quiz.should_receive(:answers_file_name).and_return(@answers_file_name)
+			File.should_receive(:open).with(@dictionary_file_name, "r").and_return(@dictionary_mock_file)
+			File.should_receive(:open).with(@questions_file_name, "w").and_return(@questions_mock_file)
+			File.should_receive(:open).with(@answers_file_name, "w").and_return(@answers_mock_file)
+			@dictionary_mock_file.should_receive(:close).and_return(true)
+			@questions_mock_file.should_receive(:close).and_return(true)
+			@answers_mock_file.should_receive(:close).and_return(true)
 		end
 
-		context "when #find_sequences is run" do
-			context "with an empty word list" do
-				before do
-					@words = []
-					@mock_file_dictionary.should_receive(:each).with(no_args()).and_yield(nil)
-					@number_of_sequences = @fast_pencil_quiz.find_sequences(@mock_file_dictionary)
-				end
-		
-				specify { @number_of_sequences.should == 0 }
+		context "with an empty word list" do
+			before(:each) do
+				@words = []
+				@dictionary_mock_file.stub(:each).with(any_args).and_return nil
+				@fast_pencil_quiz.run
+			end
+			
+			it { should_not_receive(:determine_unique_sequences) }
+	
+			context "q_and_a_candidates" do
 				subject { @fast_pencil_quiz.q_and_a_candidates }
 				it { should be_a Hash }
 				it { should be_empty }
 			end
-	
-			context "with a valid word list" do
-				before do
-					@words = %w[arrows food carrots give me food]
-					@sequences = %w[arro carr food give rots rows rrot rrow]
-					@containing_words = [
-						%w[arrows carrots],
-						%w[carrots],
-						%w[food food],
-						%w[give],
-						%w[carrots],
-						%w[arrows],
-						%w[carrots],
-						%w[arrows],
-					]
-					@mock_file_dictionary.should_receive(:each).with(no_args()).and_yield("arrows").and_yield("food").and_yield("carrots").and_yield("give").and_yield("me").and_yield("food")
-					@number_of_sequences = @fast_pencil_quiz.find_sequences(@mock_file_dictionary)
-				end
+		end
+
+		context "with a valid word list" do
+			before(:each) do
+				@words = %w[arrows food carrots give me food]
+				@sequences = %w[arro carr food give rots rows rrot rrow]
+				@unique_sequences = @sequences - %w[arro food]
+				@containing_words = [
+					%w[arrows carrots],
+					%w[carrots],
+					%w[food food],
+					%w[give],
+					%w[carrots],
+					%w[arrows],
+					%w[carrots],
+					%w[arrows],
+				]
+				@unique_words = (@containing_words - [%w[arrows carrots], %w[food food]]).flatten.sort
+				@questions = []
+				@answers   = []
+				@dictionary_mock_file.should_receive(:each).with(no_args).and_yield("arrows").and_yield("food").and_yield("carrots").and_yield("give").and_yield("me").and_yield("food")
+				@questions_mock_file.stub(:puts) {|sequence| @questions << sequence }
+				@answers_mock_file.stub(:puts) {|word| @answers << word }
+				@fast_pencil_quiz.run
+			end
 		
-				specify { @number_of_sequences.should == @sequences.size }
-			
+			context "q_and_a_candidates" do
 				subject { @fast_pencil_quiz.q_and_a_candidates }
 				it { should be_a Hash }
 				it { should_not be_empty }
 				its(:size) { should == 8 }
-			
-				its(:keys) { subject.sort.should == @sequences }
-	
-				(0...8).to_a.each do |i|
-					context "when testing sequence " + i.to_s do 
-						specify { @fast_pencil_quiz.q_and_a_candidates[@sequences[i]].should == @containing_words[i] }
-					end
-				end
-	
-				context "when #determine_unique_sequences is run" do
-					before do
-						#@unique_sequences = %w[carr give rots rows rrot rrow]
-						@unique_sequences = @sequences - %w[arro food]
-						@unique_words = @containing_words - [%w[arrows carrots], %w[food food]]
-						@questions_file_name = "path to questions_file"
-						@answers_file_name   = "path to answers_file"
-						@mock_file_questions = mock(File)
-						@mock_file_answers   = mock(File)
-						@fast_pencil_quiz.should_receive(:questions_file_name).and_return(@questions_file_name)
-						@fast_pencil_quiz.should_receive(:answers_file_name).and_return(@answers_file_name)
-						File.should_receive(:open).with(@questions_file_name, "w").and_yield(@mock_file_questions)
-						File.should_receive(:open).with(@answers_file_name, "w").and_yield(@mock_file_answers)
-						@number_of_unique_sequences = @fast_pencil_quiz.determine_unique_sequences()
-					end
+			end
 		
-					specify { @number_of_unique_sequences.should == @unique_sequences.size }
-	
-					it "should write out sequences found in only one word" do
-						(0...5).to_a.each do |i|
-							@mock_file_questions.should_receive(:puts).once().with(@unique_sequences[i])
-						end
-					end
-	
-					it "should only write enough times to include all the questions" do
-						@mock_file_questions.should_receive(:puts).exactly(5).times
-					end
-	
-					it "should write out words corresponding to unique sequences" do
-						(0...5).to_a.each do |i|
-							@mock_file_answers.should_receive(:puts).once().with(@unique_words[i])
-						end
-					end
-	
-					it "should only write enough times to include all the answers" do
-						@mock_file_answers.should_receive(:puts).exactly(5).times
-					end
+			specify { @fast_pencil_quiz.q_and_a_candidates.keys.sort.should == @sequences }
+
+			(0...8).each do |i|
+				context "when testing sequence " + i.to_s do 
+					specify { @fast_pencil_quiz.q_and_a_candidates[@sequences[i]].should == @containing_words[i] }
+				end
+			end
+
+			context "when #determine_unique_sequences is run" do
+				it "only unique sequences should be written to the questions file" do
+					@questions.sort.should == @unique_sequences
+				end
+
+				it "only words for unique suequences should be written to the answers file" do
+					@answers.sort.should == @unique_words
 				end
 			end
 		end
 	end
 end
-
 
